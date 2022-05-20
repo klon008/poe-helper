a8dfab26426693e1914b03635a91b01242301a92
Pavel <klon_008@mail.ru>
2022-05-20 18:02:36 +0700

    all fixed!

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -6,7 +6,8 @@ import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
 import axios from 'axios';
-
+import { VueCookieNext } from 'vue-cookie-next'
+import { defineComponent } from 'vue';
 </script>
 
 <template>
@@ -16,27 +17,42 @@ import axios from 'axios';
     </template>
     <template #heading>Exalted</template>
 
-    <input v-model="exalt" placeholder="Exalt to Chaos" class="flat" v-on:keypress="NumbersOnly">
-    <p>Введённое сообщение: {{ exalt }}</p>
+    <div class="row">
+    <input v-model="exaltz" v-on:change="meowMow($event)" placeholder="Exalt to Chaos" class="flat" v-on:keypress="NumbersOnly" >
+    <button style="" @click="buttonClick">Calc</button>
+    </div>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
+      <div class="chaos-text">{{chaosText}}</div>
       <img class="css-mpit4l" src="https://web.poecdn.com/image/Art/2DItems/Currency/CurrencyRerollRare.png?scale=1&w=1&h=1" alt="">
     </template>
     <template #heading>Chaos orb</template>
 
     <input v-model="chaoses" placeholder="Chaoses" class="flat" disabled>
   </WelcomeItem>
-  <WelcomeItem> 
-    <textarea name="aaa" id="" cols="30" rows="10">{{sjson}}</textarea>
-  </WelcomeItem>
 </template>
 
 <style scoped>
+.chaos-text{
+  position: absolute;
+  top: -25px;
+  left: 0px;
+  z-index: 11111111111;
+  color: white;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgb(192, 192, 192);
+}
+.row{
+  display: flex;
+}
+.row > *:first-child {
+  margin-right: 5%;
+}
 input.flat {
 	background-color: var(--vt-c-text-light-2);
-	color: var(--color-text);
+	color: var(--vt-c-white-soft);
 	outline-color: var(--vt-c-text-dark-2);
     border-radius: 3px;
     padding: 15px 10px;
@@ -142,23 +158,25 @@ input.flat-checkbox:checked ~ label:after,
 </style>
 
 <script>
-export default {
+export default defineComponent({
   name: "HelloWorld",
   data() {
     return {
-      exalt: "",
+      exaltz: "",
       chaoses: "",
       sjson: "",
+      chaosText: "",
     }
   },
   watch: {
-    exalt(newQuestion, oldQuestion) {
+    exaltz(newQuestion, oldQuestion) {
       const numbersReg = /(?<=^| )\d+(\.\d+)?(?=$| )|(?<=^| )\.\d+(?=$| )/g;
-      if (newQuestion.length > 0 && newQuestion !== oldQuestion){
-        this.chaoses = newQuestion.match(numbersReg);
-        if (this.chaoses){
-          axios.get('https://poe.ninja/api/data/currencyoverview?league=Sentinel&type=Currency').then((response) => this.sjson = response.data.response)
-          .catch((error) => console.log(error.response.data));          
+      if (newQuestion.length > 0 && newQuestion !== oldQuestion && this.chaosText){
+        const exalts = newQuestion.match(numbersReg);
+        if (exalts){
+          this.chaoses = this.chaosText * exalts
+          //axios.get('https://poe.ninja/api/data/currencyoverview?league=Sentinel&type=Currency').then((response) => this.sjson = response.data.response)
+          //.catch((error) => console.log(error.response.data));          
         }
       }
     },
@@ -172,7 +190,57 @@ export default {
       } else {
         return true;
       }
+    },
+    meowMow(dam){
+      var exa = this.exaltz;
+      const numbersReg = /(?<=^| )\d+(\.\d+)?(?=$| )|(?<=^| )\.\d+(?=$| )/g;
+      if (exa.length > 0 ){
+        this.chaoses = exa.match(numbersReg) * exalts;
+        if (this.chaoses){
+          let axconfig = {
+              "sell": "Chaos Orb",
+              "buy": "Exalted Orb",
+              "limit": 5
+          }
+          //axios.get('https://poe.ninja/api/data/currencyoverview', axconfig).then((response) => this.sjson = response.data.response)
+          axios({
+            method: "get",
+            url:'https://raw.githubusercontent.com/The-Forbidden-Trove/poeninja-prices/main/League/Currency.txt',
+            data: axconfig,
+            headers: { "Content-Type": "multipart/form-data" },
+
+          }).then((response) => {
+            console.log(response);
+            this.sjson = response.data;
+            const tempJson = response.data;
+            const selectedEx = tempJson.find(o => o.name == 'Exalted Orb')
+            const chaosEquivalent = selectedEx.chaosEquivalent;
+            this.chaosText = chaosEquivalent;
+            VueCookieNext.setCookie('chaosEquivalent', chaosEquivalent, { 
+              expire: '1d'
+            });
+
+            }
+          )
+          .catch(
+            (error) => console.log(error.response)
+            );          
+        }
+      }
+    },
+    buttonClick(){
+    },
+    
+  },
+  mounted(){
+      this.$nextTick(() => {
+        console.log('11')
+        let loadCookie = VueCookieNext.getCookie('chaosEquivalent');
+        if (!loadCookie){
+          loadCookie = 100;
+        }
+        this.chaosText = loadCookie
+      })
     }
-  }
-}
+})
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { VueCookieNext } from 'vue-cookie-next'
 
 const app = createApp(App)
-app.use(VueAxios, axios) // 👈
+app.use(VueAxios, axios ) // 👈
+app.use(VueCookieNext)
 app.mount('#app')
